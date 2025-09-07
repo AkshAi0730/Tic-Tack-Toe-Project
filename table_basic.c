fa5ebc9da9dfba55736c57ea9458f2bf8a177be2
AkshAi0730 <it25100933@my.sliit.lk>
2025-09-07 10:49:50 +0530

    Basic_Table

---
+++ table_basic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+	//Printing Basic Table 3 X 3
+	printf("-------------------\n");
+	printf(" |    |    |    |  \n");
+	printf("-------------------\n");
+	printf(" |    |    |    |  \n");
+	printf("-------------------\n");
+	printf(" |    |    |    |  \n");
+	printf("-------------------\n");
+
+	return 0;
+}
