bc14e3769dcbdb1ff0366d8c0231d79df03b892d
AkshAi0730 <it25100933@my.sliit.lk>
2025-09-11 13:10:06 +0530

    full table

--- table.c
+++ table.c
@@ -2,6 +2,7 @@
 
 int l, c, n;
 
+//Create Vertical line for table
 int line(int size){
 	printf("   -");
 	l = 0;
@@ -11,6 +12,8 @@ int line(int size){
 	}while(size > l);
 	printf("--\n");
 }
+
+//Create Horizontal line for table
 int horizontal(int size, int table[][size], int row){
 	printf(" %d  |", (row+1));
 	c = 0;
@@ -22,12 +25,27 @@ int horizontal(int size, int table[][size], int row){
 	return 0;
 }
 
+//Include  coloumn number
+int col_no(int coloumn){
+	int no = 1;
+	printf("      ");
+	do{
+		printf("%d   ", no);
+		no++;
+	}while(coloumn >= no);
+	printf(" \n");
+	return 0;
+}
+
 int main(){
+
+	//Get the size of table
 	int size;
 	printf("Enter Table Size (3 - 9):\n");
 	scanf("%d", &size);
 	l = 0;
-
+	
+	//intialize table with 2D array
 	if(size >= 3 && size <= 9){
 		int table[size][size];
 		for(int i = 0; i<size; i++){
@@ -37,6 +55,11 @@ int main(){
 		}
 		n = 0;
 	
+
+		//print row numbers over the table
+		col_no(size);
+
+		//Print the output table
 		do {
 			line(size);
 			horizontal(size,table,n);
@@ -44,7 +67,11 @@ int main(){
 		} while (size > n);
 		l = 0;
 		line(size);
-	} else{
+
+	}
+
+	//Ask for new input if input is wrong
+       else{
 		printf("Enter a Number between 3 and 9 \n");
 		printf("\n");
 		main();
